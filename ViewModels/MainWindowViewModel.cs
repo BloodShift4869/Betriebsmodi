0f20e6f70ae34949f0ee30b1b8595ec2ac1d1343
BloodShift4869 <lasse.koehn96@web.de>
2025-07-21 00:12:21 +0200

    Created base layout for main window

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,8 @@
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string Mode { get; } = "ECB";
+    public string Cores { get; } = "1";
+    public string MsWhole { get; } = "2";
+    public string MsDecimal { get; } = "41";
 }
